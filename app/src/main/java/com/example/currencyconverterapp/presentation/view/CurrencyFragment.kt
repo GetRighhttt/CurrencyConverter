e4c4534ea681cde09ec1f19ab2c66c45b4ae1543
stefan <stefanbayne@gmail.com>
2022-10-26 14:13:30 -0400

    Final anims added before backend dev.

--- app/src/main/java/com/example/currencyconverterapp/presentation/view/CurrencyFragment.kt
+++ app/src/main/java/com/example/currencyconverterapp/presentation/view/CurrencyFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.currencyconverterapp.R
+import com.example.currencyconverterapp.databinding.FragmentCurrencyBinding
 
 
 class CurrencyFragment : Fragment() {
+
+    private var _binding: FragmentCurrencyBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_currency, container, false)
+        _binding = FragmentCurrencyBinding.inflate(inflater, container, false)
+
+        return binding!!.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

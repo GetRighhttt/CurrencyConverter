f21ee5db2e2c7e9d1166ba863db0ba483118e88a
stefan bayne <stefanbayne@gmail.com>
2024-03-27 10:29:13 -0400

    fix: refactor companion object

--- app/src/main/java/com/example/currencyconverterapp/presentation/viewmodel/CurrencyViewModel.kt
+++ app/src/main/java/com/example/currencyconverterapp/presentation/viewmodel/CurrencyViewModel.kt
@@ -49,11 +49,9 @@ class CurrencyViewModel @Inject constructor(
             _conversion.value = CurrencyEvent.Loading
             _isLoading.value = true
 
-            when (val ratesResponse =
-                repository.getRates(fromCountryCurrency)) {
+            when (val ratesResponse = repository.getRates(fromCountryCurrency)) {
                 is Resource.Error -> _conversion.value =
                     CurrencyEvent.Failure(ratesResponse.message!!)
-
                 is Resource.Success -> {
                     val rates = ratesResponse.data!!.rates
                     val rate = getRateForCurrency(toCountryCurrency, rates)
@@ -75,40 +73,42 @@ class CurrencyViewModel @Inject constructor(
         }
     }
 
-    private fun getRateForCurrency(currency: String, rates: Rates) =
-        when (currency) {
-            "CAD" -> rates.cAD
-            "HKD" -> rates.hKD
-            "ISK" -> rates.iSK
-            "EUR" -> rates.eUR
-            "PHP" -> rates.pHP
-            "DKK" -> rates.dKK
-            "HUF" -> rates.hUF
-            "CZK" -> rates.cZK
-            "AUD" -> rates.aUD
-            "RON" -> rates.rON
-            "SEK" -> rates.sEK
-            "IDR" -> rates.iDR
-            "INR" -> rates.iNR
-            "BRL" -> rates.bRL
-            "RUB" -> rates.rUB
-            "HRK" -> rates.hRK
-            "JPY" -> rates.jPY
-            "THB" -> rates.tHB
-            "CHF" -> rates.cHF
-            "SGD" -> rates.sGD
-            "PLN" -> rates.pLN
-            "BGN" -> rates.bGN
-            "CNY" -> rates.cNY
-            "NOK" -> rates.nOK
-            "NZD" -> rates.nZD
-            "ZAR" -> rates.zAR
-            "USD" -> rates.uSD
-            "MXN" -> rates.mXN
-            "ILS" -> rates.iLS
-            "GBP" -> rates.gBP
-            "KRW" -> rates.kRW
-            "MYR" -> rates.mYR
-            else -> null
-        }
+    companion object {
+        private fun getRateForCurrency(currency: String, rates: Rates) =
+            when (currency) {
+                "CAD" -> rates.cAD
+                "HKD" -> rates.hKD
+                "ISK" -> rates.iSK
+                "EUR" -> rates.eUR
+                "PHP" -> rates.pHP
+                "DKK" -> rates.dKK
+                "HUF" -> rates.hUF
+                "CZK" -> rates.cZK
+                "AUD" -> rates.aUD
+                "RON" -> rates.rON
+                "SEK" -> rates.sEK
+                "IDR" -> rates.iDR
+                "INR" -> rates.iNR
+                "BRL" -> rates.bRL
+                "RUB" -> rates.rUB
+                "HRK" -> rates.hRK
+                "JPY" -> rates.jPY
+                "THB" -> rates.tHB
+                "CHF" -> rates.cHF
+                "SGD" -> rates.sGD
+                "PLN" -> rates.pLN
+                "BGN" -> rates.bGN
+                "CNY" -> rates.cNY
+                "NOK" -> rates.nOK
+                "NZD" -> rates.nZD
+                "ZAR" -> rates.zAR
+                "USD" -> rates.uSD
+                "MXN" -> rates.mXN
+                "ILS" -> rates.iLS
+                "GBP" -> rates.gBP
+                "KRW" -> rates.kRW
+                "MYR" -> rates.mYR
+                else -> null
+            }
+    }
 }
